7814cffb173ab1ebaa1df3d0a278a84ce3100888
Iryna Gumeniuk <iriwkagumenuk@gmail.com>
2021-10-23 12:57:55 +0200

    hooks

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
-import React, { Component } from "react";
+import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import Phonebook from "./phonebook/Phonebook";
 import ContactList from "./contactList/ContactList";
 
-class App extends Component {
-  state = {
-    contacts: [
-      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-    ],
-    filter: "",
-    name: "",
-    number: "",
-  };
+export default function App() {
 
-  handleSubmit = (e) => {
+  const [contacts, setContacts] = useState([
+    { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+    { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+    { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+    { id: "id-4", name: "Annie Copeland", number: "227-91-26" }
+  ]);
+
+  const [filter, setFilter] = useState("");
+  const [name, setName] = useState("");
+  const [number, setNumber] = useState("");
+
+
+  const handleSubmit = (e) => {
     e.preventDefault();
     const contact = {
       id: uuidv4(),
-      name: this.state.name,
-      number: this.state.number,
+      name: name,
+      number: number,
     };
-    this.state.contacts.find(
+    contacts.find(
       ({ name }) => name === contact.name && contact.name
     )
       ? alert(`${contact.name} already exists`)
@@ -35,46 +36,41 @@ class App extends Component {
     this.setState({ name: "", number: "" });
   };
 
-  handleChange = (e) => {
+  const handleChange = (e) => {
     const { name, value } = e.target;
-    this.setState({ [name]: value });
+    setName({ [name]: value });
   };
 
-  filterByName = () => {
-    return this.state.contacts.filter((contact) =>
-      contact.name.toLowerCase().includes(this.state.filter.toLowerCase())
+  const filterByName = () => {
+    return filter((contact) =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
     );
   };
 
-  deleteContact = (id) => {
-    this.setState((prev) => {
+  const deleteContact = (id) => {
+    setContacts((prev) => {
       return {
         contacts: prev.contacts.filter((contact) => contact.id !== id),
       };
     });
   };
 
-  render() {
-    const filterContact = this.filterByName();
-    const { filter } = this.state;
-    return (
-      <>
-        <Phonebook
-          onSubmit={this.handleSubmit}
-          onChange={this.handleChange}
-          name={this.state.name}
-          number={this.state.number}
-        />
+  const filterContact = filterByName();
+  return (
+    <>
+      <Phonebook
+        onSubmit={handleSubmit}
+        onChange={handleChange}
+        name={name}
+        number={number}
+      />
 
-        <ContactList
-          contacts={filterContact}
-          filter={filter}
-          onChange={this.handleChange}
-          deleteContact={this.deleteContact}
-        />
-      </>
-    );
-  }
+      <ContactList
+        contacts={filterContact}
+        filter={filter}
+        onChange={handleChange}
+        deleteContact={deleteContact}
+      />
+    </>
+  );
 }
-
-export default App;
